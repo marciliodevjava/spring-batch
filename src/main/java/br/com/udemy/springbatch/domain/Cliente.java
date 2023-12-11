856499343aefe0e52ea60c086164e7d9a649cf99
marcsilva <marciliodevjava@gmail.com>
2023-12-11 17:21:41 -0300

    Create Cliente

---
+++ src/main/java/br/com/udemy/springbatch/domain/Cliente.java
@@ -0,0 +1,40 @@
+package br.com.udemy.springbatch.domain;
+
+public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
