dd4415d2d65e97097d3b8fa0391c618b82d6b09e
marcsilva <marciliodevjava@gmail.com>
2023-12-12 15:38:15 -0300

    Create

---
+++ src/main/java/br/com/udemy/springbatch/tasklet/ProcessamentoQuinzeDiasTasklet.java
@@ -0,0 +1,17 @@
+package br.com.udemy.springbatch.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProcessamentoQuinzeDiasTasklet implements Tasklet {
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("Inicio processamento Quinze Dias!");
+        System.out.println("Fim processamento Quinze Dias");
+        return RepeatStatus.FINISHED;
+    }
+}
